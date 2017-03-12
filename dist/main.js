df4484083f099db042177daedbd8a4e49a09d274
kagami <kagami.amano@gmail.com>
2017-03-12 15:02:40 +0800

    add landing page, nodeImg & backgroundImg become changeable

--- dist/main.js
+++ dist/main.js
@@ -10,8 +10,83 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 // Componets
 
-var Page = function (_React$Component) {
-  _inherits(Page, _React$Component);
+var TlNode = function (_React$Component) {
+  _inherits(TlNode, _React$Component);
+
+  function TlNode() {
+    _classCallCheck(this, TlNode);
+
+    return _possibleConstructorReturn(this, (TlNode.__proto__ || Object.getPrototypeOf(TlNode)).apply(this, arguments));
+  }
+
+  _createClass(TlNode, [{
+    key: "render",
+    value: function render() {
+      var nodeStyle = {
+        backgroundImage: "url(" + this.props.img + ")",
+        backgroundSize: "cover"
+      };
+      return React.createElement("div", { className: "zt-tl-node", style: nodeStyle });
+    }
+  }]);
+
+  return TlNode;
+}(React.Component);
+
+// TODO: 编辑内容
+
+
+var LandingPage = function (_React$Component2) {
+  _inherits(LandingPage, _React$Component2);
+
+  function LandingPage() {
+    _classCallCheck(this, LandingPage);
+
+    return _possibleConstructorReturn(this, (LandingPage.__proto__ || Object.getPrototypeOf(LandingPage)).apply(this, arguments));
+  }
+
+  _createClass(LandingPage, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        { className: "section" },
+        React.createElement(
+          "div",
+          { className: "zt-landing-page" },
+          React.createElement(
+            "div",
+            { className: "zt-top" },
+            React.createElement(
+              "div",
+              { className: "zt-tl-header" },
+              React.createElement(
+                "div",
+                { className: "zt-tl-message" },
+                React.createElement(
+                  "h2",
+                  null,
+                  "\u5468\u6D9B\u7684\u5947\u5999\u65C5\u7A0B"
+                ),
+                React.createElement(
+                  "h3",
+                  null,
+                  "TaoTao's Marvellous Adventure"
+                )
+              )
+            )
+          ),
+          React.createElement(TlNode, { img: "img/icon_2.png" })
+        )
+      );
+    }
+  }]);
+
+  return LandingPage;
+}(React.Component);
+
+var Page = function (_React$Component3) {
+  _inherits(Page, _React$Component3);
 
   function Page() {
     _classCallCheck(this, Page);
@@ -22,13 +97,18 @@ var Page = function (_React$Component) {
   _createClass(Page, [{
     key: "render",
     value: function render() {
+      var pageStyle = {
+        backgroundImage: "url(" + this.props.node.bgImg + ")",
+        backgroundSize: "cover"
+      };
+
       return React.createElement(
         "div",
-        { className: "section" },
+        { className: "section", style: pageStyle },
         React.createElement(
           "div",
           { className: "zt-tl-block" },
-          React.createElement("div", { className: "zt-tl-node" }),
+          React.createElement(TlNode, { img: this.props.node.nodeImg }),
           React.createElement(
             "div",
             { className: "zt-tl-content left-content" },
@@ -65,72 +145,79 @@ var Page = function (_React$Component) {
   return Page;
 }(React.Component);
 
-var Pages = function (_React$Component2) {
-  _inherits(Pages, _React$Component2);
+var App = function (_React$Component4) {
+  _inherits(App, _React$Component4);
 
-  function Pages() {
-    _classCallCheck(this, Pages);
+  function App() {
+    _classCallCheck(this, App);
 
-    return _possibleConstructorReturn(this, (Pages.__proto__ || Object.getPrototypeOf(Pages)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
   }
 
-  _createClass(Pages, [{
+  _createClass(App, [{
     key: "render",
     value: function render() {
       return React.createElement(
         "div",
         null,
+        React.createElement(LandingPage, null),
         this.props.pages
       );
     }
   }]);
 
-  return Pages;
+  return App;
 }(React.Component);
 
 // Dummy data
 
-var node1 = {
+var page1 = {
   id: "1",
+  nodeImg: "img/icon_1.png",
+  bgImg: "img/bg_1.png",
   date: "2015",
   title: "真情无限",
   description: "环保环保环保",
-  thumbnail: "img/thumbnail.jpg",
+  thumbnail: "img/thumbnail_2.jpg",
   link: "http://www.bilibili.com/video/av5202457/index_2.html"
 };
 
-var node2 = {
+var page2 = {
   id: "2",
+  nodeImg: "img/icon_2.png",
+  bgImg: "img/bg_2.png",
   date: "2016",
   title: "春节联欢晚会2016",
   description: "2016央视春晚主持阵容发布：在中央电视台一号演播大厅主会场的共有六位主持人，分别是周涛、朱军、董卿、撒贝宁、李思思和尼格买提。福建泉州分会场：李佳明、赵琳硕；陕西西安分会场：朱迅、徐杰；广东广州分会场：任鲁豫、邓璐；内蒙古呼伦贝尔分会场：马跃、欧仁图雅。",
-  thumbnail: "img/thumbnail.jpg",
+  thumbnail: "img/thumbnail_1.jpg",
   link: "http://www.bilibili.com/video/av5202457/index_1.html"
 };
 
-var node3 = {
+var page3 = {
   id: "3",
+  nodeImg: "img/icon_1.png",
+  bgImg: "img/bg_1.png",
   date: "2017",
   title: "Dream",
   description: "BLABLABLABLABLABLABL",
-  thumbnail: "img/thumbnail.jpg",
+  thumbnail: "img/thumbnail_2.jpg",
   link: "http://www.bilibili.com/video/av5202457/index_3.html"
 };
 
-var nodeList = [node1, node2, node3];
-
-var pageList = nodeList.map(function (node) {
-  return React.createElement(Page, { key: node.id, node: node });
-});
+var pageList = [page1, page2, page3];
 
 // Render
 
-ReactDOM.render(React.createElement(Pages, { pages: pageList }), document.getElementById('fullpage'));
+var pages = pageList.map(function (node) {
+  return React.createElement(Page, { key: node.id, node: node });
+});
+
+ReactDOM.render(React.createElement(App, { pages: pages }), document.getElementById('fullpage'));
 
 // Animation & Action
 
 $('#fullpage').fullpage({
   anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-  sectionsColor: ['#7E8F7C', '#C63D0F', '#1BBC9B'],
+  // sectionsColor: ['#7E8F7C', '#C63D0F', '#1BBC9B'],
   scrollBar: true
 });
--- src/main.js
+++ src/main.js
@@ -1,13 +1,53 @@
 
 // Componets
 
-class Page extends React.Component {
-  render() {
+class TlNode extends React.Component {
+  render () {
+    const nodeStyle = {
+      backgroundImage: "url(" + this.props.img + ")",
+      backgroundSize: "cover",
+    };
+    return (
+      <div className="zt-tl-node" style={nodeStyle}>
+      </div>
+    )
+  }
+}
+
+// TODO: 编辑内容
+class LandingPage extends React.Component {
+  render () {
     return (
       <div className="section">
-          <div className="zt-tl-block">
-              <div className="zt-tl-node">
+          <div className="zt-landing-page">
+              <div className="zt-top">
+                  <div className="zt-tl-header">
+                      <div className="zt-tl-message">
+                          <h2>周涛的奇妙旅程</h2>
+                          <h3>TaoTao's Marvellous Adventure</h3>
+                      </div>
+                  </div>
               </div>
+
+              <TlNode img="img/icon_2.png" />
+          </div>
+      </div>
+    )
+  }
+}
+
+
+class Page extends React.Component {
+  render() {
+    const pageStyle = {
+      backgroundImage: "url(" + this.props.node.bgImg + ")",
+      backgroundSize: "cover",
+    };
+
+    return (
+      <div className="section" style={pageStyle}>
+          <div className="zt-tl-block">
+              <TlNode img={this.props.node.nodeImg} />
               <div className="zt-tl-content left-content">
                   <h4 className="zl-tl-date">{this.props.node.date}</h4>
                   <h3 className="zl-tl-title">{this.props.node.title}</h3>
@@ -25,10 +65,11 @@ class Page extends React.Component {
   }
 }
 
-class Pages extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
+        <LandingPage />
         {this.props.pages}
       </div>
     )
@@ -39,45 +80,50 @@ class Pages extends React.Component {
 
 // Dummy data
 
-const node1 = {
+const page1 = {
   id: "1",
+  nodeImg: "img/icon_1.png",
+  bgImg: "img/bg_1.png",
   date: "2015",
   title: "真情无限",
   description: "环保环保环保",
-  thumbnail: "img/thumbnail.jpg",
+  thumbnail: "img/thumbnail_2.jpg",
   link: "http://www.bilibili.com/video/av5202457/index_2.html"
 };
 
-const node2 = {
+const page2 = {
   id: "2",
+  nodeImg: "img/icon_2.png",
+  bgImg: "img/bg_2.png",
   date: "2016",
   title: "春节联欢晚会2016",
   description: "2016央视春晚主持阵容发布：在中央电视台一号演播大厅主会场的共有六位主持人，分别是周涛、朱军、董卿、撒贝宁、李思思和尼格买提。福建泉州分会场：李佳明、赵琳硕；陕西西安分会场：朱迅、徐杰；广东广州分会场：任鲁豫、邓璐；内蒙古呼伦贝尔分会场：马跃、欧仁图雅。",
-  thumbnail: "img/thumbnail.jpg",
+  thumbnail: "img/thumbnail_1.jpg",
   link: "http://www.bilibili.com/video/av5202457/index_1.html"
 };
 
-const node3 = {
+const page3 = {
   id: "3",
+  nodeImg: "img/icon_1.png",
+  bgImg: "img/bg_1.png",
   date: "2017",
   title: "Dream",
   description: "BLABLABLABLABLABLABL",
-  thumbnail: "img/thumbnail.jpg",
+  thumbnail: "img/thumbnail_2.jpg",
   link: "http://www.bilibili.com/video/av5202457/index_3.html"
 };
 
-const nodeList = [node1, node2, node3];
-
-
-const pageList = nodeList.map( (node) =>
-  <Page key={node.id} node={node} />
-);
+const pageList = [page1, page2, page3];
 
 
 // Render
 
+const pages = pageList.map( (node) =>
+  <Page key={node.id} node={node} />
+);
+
 ReactDOM.render(
-  <Pages pages={pageList}/>,
+  <App pages={pages}/>,
   document.getElementById('fullpage')
 );
 
@@ -87,6 +133,6 @@ ReactDOM.render(
 
 $('#fullpage').fullpage({
     anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-    sectionsColor: ['#7E8F7C', '#C63D0F', '#1BBC9B'],
+    // sectionsColor: ['#7E8F7C', '#C63D0F', '#1BBC9B'],
     scrollBar: true
 });
