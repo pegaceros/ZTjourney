bf70bd125e8ddcc556c866132603429b4923414f
kagami <kagami.amano@gmail.com>
2017-03-17 11:53:27 +0800

    rename source files into lowercase

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,17 @@ var gulp = require('gulp');
 // var coffee = require('gulp-coffee');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var rename = require("gulp-rename");
 
 gulp.task('default', function() {
-  // 默认的任务代码放在这
+    // 默认的任务代码放在这
 });
 
 
-gulp.task('sass', function () {
-  gulp.src('./src/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./package/dist/'));
+gulp.task('sass', function() {
+    gulp.src('./src/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./package/dist/'));
 });
 
 // gulp.task('coffee', function() {
@@ -21,19 +22,24 @@ gulp.task('sass', function () {
 //     .pipe(gulp.dest('./src/'));
 // });
 
-gulp.task("babel", function () {
-  gulp.src("./src/**/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("./package/dist/"));
+gulp.task("babel", function() {
+    gulp.src("./src/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("./package/dist/"));
 });
 
 gulp.task('watch', ['babel', 'sass'], function() {
-  gulp.watch('./src/**/*.js', ['babel']);
-  gulp.watch('./src/**/*.scss', ['sass']);
+    gulp.watch('./src/**/*.js', ['babel']);
+    gulp.watch('./src/**/*.scss', ['sass']);
 });
 
 
-gulp.task('copy',  function() {
-  gulp.src('./package/**/*')
-    .pipe(gulp.dest("../ZhouTao/"))
+gulp.task('copy', function() {
+    gulp.src('./package/**/*')
+        .pipe(rename(function(path) {
+            path.dirname = path.dirname.toLowerCase();
+            path.basename = path.basename.toLowerCase();
+            path.extname = path.extname.toLowerCase();
+        }))
+        .pipe(gulp.dest("../ZhouTao/"));
 });
